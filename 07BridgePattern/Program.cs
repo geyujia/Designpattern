b5dcc721bf83c0e201167f7c480ff2a94caf00dd
geyujia <1956681329@qq.com>
2021-01-16 22:38:00 +0800

    #结构型模式示例#

---
+++ 07BridgePattern/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+#region 场景描述
+
+#endregion
+
+/// <summary>
+/// 07-结构型：桥接模式
+/// </summary>
+namespace _07BridgePattern
+{
+    class Program
+    {
+        /// <summary>
+        /// 桥接模式演示
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //TEST
+            Shape redCircle = new Circle(100, 100, 200, new RedCircle());
+            Shape greenCircle = new Circle(100, 100, 200, new GreenCircle());
+            redCircle.Draw();
+            greenCircle.Draw();
+            Console.ReadKey();
+        }
+    }
+
+    #region 基础类构建
+    /// <summary>
+    /// 步骤 1 创建桥接实现接口。
+    /// </summary>
+    public interface DrawAPI
+    { 
+        void DrawCircle(int radius, int x, int y);
+    }
+    /// <summary>
+    /// 步骤 2  创建实现了 DrawAPI 接口的实体桥接实现类。
+    /// </summary>
+    public class RedCircle : DrawAPI
+    {
+        public void DrawCircle(int radius, int x, int y)
+        {
+            Console.WriteLine("Drawing Circle[ color: red, radius: " + radius + ", x: " + x + ", y:" + y + "]");
+        }
+    }
+
+    public class GreenCircle : DrawAPI
+    {
+        public void DrawCircle(int radius, int x, int y)
+        {
+            Console.WriteLine("Drawing Circle[ color: green, radius: " + radius + ", x: " + x + ",y: " + y + "]");
+        }
+    }
+
+    /// <summary>
+    /// 步骤 3 使用 DrawAPI 接口创建抽象类 Shape。
+    /// </summary>
+    public abstract class Shape
+    {
+        protected  DrawAPI drawAPI;
+
+        public Shape(DrawAPI drawAPI)
+        {
+            this.drawAPI = drawAPI;
+        }
+
+        public abstract void Draw();
+    }
+    /// <summary>
+    /// 步骤 4 创建实现了 Shape 接口的实体类。
+    /// </summary>
+    public class Circle:Shape
+    {
+        private int x, y, radius;
+
+        public Circle(int x, int y, int radius, DrawAPI drawAPI) : base(drawAPI)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            drawAPI.DrawCircle(radius,x,y);
+        }
+    }
+
+    #endregion
+}
---
+++ 08DecoratorPattern/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+#region 场景描述
+
+#endregion
+
+/// <summary>
+/// 08-结构型：装饰器模式
+/// </summary>
+namespace _08DecoratorPattern
+{
+    class Program
+    {
+        /// <summary>
+        /// 使用 RedShapeDecorator 来装饰 Shape 对象。
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle();//里氏替换原则
+            Shape redCircle = new RedShapeDecorator(new Circle());
+            Shape redRectangle = new RedShapeDecorator(new Rectangle());
+
+            Console.WriteLine("Circle with normal border");
+            circle.Draw();
+
+            Console.WriteLine("\nCircle of red border");
+            redCircle.Draw();
+
+            Console.WriteLine("\nRectangle of red border");
+            redRectangle.Draw();
+
+            Console.ReadKey();
+        }
+    }
+
+    #region 基础类构建
+    /// <summary>
+    /// 步骤 1 创建形状抽象类。
+    /// </summary>
+    public abstract class Shape
+    {
+        public abstract void Draw();
+    }
+    /// <summary>
+    /// 步骤 2 创建实现接口的实体类。
+    /// </summary>
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Shape:Rectangle.");
+
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Shape:Circle.");
+
+        }
+    }
+    /// <summary>
+    /// 步骤 3 创建实现了 Shape 接口的抽象装饰类。
+    /// </summary>
+    public abstract class ShapeDecorator : Shape
+    {
+        protected Shape decoratedShape;
+
+        public ShapeDecorator(Shape shape)
+        {
+            this.decoratedShape = shape;
+        }
+        public override void Draw()
+        {
+            decoratedShape.Draw();
+        }
+    }
+    /// <summary>
+    /// 步骤 4 创建扩展了 ShapeDecorator 类的实体装饰类。
+    /// </summary>
+    public class RedShapeDecorator : ShapeDecorator
+    {
+        public RedShapeDecorator(Shape decoratedShape) :base(decoratedShape)
+        {
+            
+        }
+
+        public override void Draw()
+        {
+            decoratedShape.Draw();
+            setRedBorder(decoratedShape);
+
+        }
+
+        private void setRedBorder(Shape decoratedShape)
+        {
+            Console.WriteLine("Border Color: Red");
+        }
+    }
+    #endregion
+}
